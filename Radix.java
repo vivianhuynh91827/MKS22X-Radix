9185b9c81f555897454598329f71d64affa403ce
vivianhuynh91827 <vhuynh00@stuy.edu>
2019-04-02 10:39:31 -0400

    first round of adding elements to buckets by one digit done

--- Radix.java
+++ Radix.java
@@ -3,21 +3,24 @@ import java.io.*;
 
 public class Radix {
   public static void radixsort(int[] data) {
-    @SuppressWarnings("unchecked")
+    @SuppressWarnings("rawtypes")
     MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
-    for (int i = 0 ; i < data.length; i ++) {
-      int digit = Math.abs( data[i] % 10 );
-      if (data[i] < 0) {
-        buckets[9-digit].add(data[i]);
+    for (int i = 0; i < 20; i ++) {
+      buckets[i] = new MyLinkedList<Integer>(); //initialize LinkedLists for each bucket
+    }
+    for (int i = 0 ; i < data.length; i ++) { //for each element in data
+      int digit = Math.abs( data[i]) %10; //retrieve ones digit of the element
+      if (data[i] < 0) { //if element is negative
+        buckets[9-digit].add(data[i]); //add to respective bucket on the left of the buckets
       }
-      else {
-        buckets[digit+10].add(data[i]);
+      else { //if element is positive
+        buckets[digit+10].add(data[i]); //add to respective bucket on the right of the buckets
       }
     }
-    System.out.println(Arrays.toString(buckets));
+    // System.out.println(Arrays.toString(buckets));
   }
   public static void main(String[] args) {
-    int[] test = {9,8,7,6,5,4,3,2,1,0,-1,-2,-3,-4,-5,-6,-7,-8,-9};
+    int[] test = {9,9,8,7,6,5,4,3,2,1,0,-1,-2,-3,-4,-5,-6,-7,-8,-9};
     radixsort(test);
   }
 }
