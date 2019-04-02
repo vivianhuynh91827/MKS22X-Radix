22372017cceb009d10915e7cb2b9e7b1f8978e5e
vivianhuynh91827 <vhuynh00@stuy.edu>
2019-04-02 10:54:53 -0400

    method added to connect lists after every loop through

--- Radix.java
+++ Radix.java
@@ -3,11 +3,18 @@ import java.io.*;
 
 public class Radix {
   public static void radixsort(int[] data) {
-    @SuppressWarnings("rawtypes")
+    @SuppressWarnings("unchecked")
     MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
-    for (int i = 0; i < 20; i ++) {
+    for (int i = 0; i < buckets.length; i ++) {
       buckets[i] = new MyLinkedList<Integer>(); //initialize LinkedLists for each bucket
     }
+    int largest = 0;
+    for (int i = 0 ; i < data.length; i ++) {
+      int abs = Math.abs(data[i]);
+      if (abs > largest) largest = abs;
+    }
+    int loops = countDigits(largest); //number of times needed to loop through in order to sort
+    boolean sorted = true; //whether or not
     for (int i = 0 ; i < data.length; i ++) { //for each element in data
       int digit = Math.abs( data[i]) %10; //retrieve ones digit of the element
       if (data[i] < 0) { //if element is negative
@@ -17,10 +24,23 @@ public class Radix {
         buckets[digit+10].add(data[i]); //add to respective bucket on the right of the buckets
       }
     }
+    int currentPlace = 2; //from the left; 1 - ones place, 2 - tens place, 3 - hundreds place, etc.
     // System.out.println(Arrays.toString(buckets));
   }
+
+  private static MyLinkedList<Integer> connectLists(MyLinkedList<Integer>[] lists) {
+    MyLinkedList<Integer> finalList = lists[0]; //starting list
+    for (int i = 1; i < lists.length; i ++) { //for each linkedlist
+      finalList.extend(lists[i]); //add it to finalList
+    }
+    return finalList;
+  }
+
+  private static int countDigits(int n) {
+    return 0;
+  }
   public static void main(String[] args) {
-    int[] test = {9,9,8,7,6,5,4,3,2,1,0,-1,-2,-3,-4,-5,-6,-7,-8,-9};
+    int[] test = {10,5,-20,-34,75,345,23,0,30,7,2,1,88};
     radixsort(test);
   }
 }
