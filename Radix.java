8db1432bbb188b566a0803b132e3d2b4ab5a11ff
vivianhuynh91827 <vhuynh00@stuy.edu>
2019-04-04 03:36:44 -0400

    radix sort completed, not yet tested for ratios

--- Radix.java
+++ Radix.java
@@ -43,6 +43,34 @@ public class Radix {
   //   }
   //   // System.out.println(currentList);
   // }
+  public static void radixsort(int[] data) {
+    @SuppressWarnings("unchecked")
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[10];
+    initializeBuckets(buckets);
+    MyLinkedList<Integer> currentList = new MyLinkedList<Integer>();
+    int largest = 0;
+    for (int i = 0 ; i < data.length; i ++) {
+      currentList.add(data[i]); //tranfer data elements over to currentList
+      int abs = Math.abs(data[i]); //find abs value of the current element
+      if (abs > largest) largest = abs; //if its greater than the current largest, replace it
+    }
+    int loops = countDigits(largest) + 1;
+    int currentLoop = 0;
+    while (currentLoop <= loops) {
+      for (int i = 0; i < currentList.size(); i ++) { //for each element in currentList
+        int current = currentList.get(i);
+        int digit = getDigit(current, currentLoop); //retrieve digit according to currentLoop
+        //if current is negative, add it to the front of the respective bucket
+        if (current < 0) buckets[digit].add(0, current);
+        //if current is positive, add it to the end of the respective bucket
+        else buckets[digit].add(current);
+      }
+      currentList = connectLists(buckets);
+      initializeBuckets(buckets);
+      currentLoop++;
+    }
+    System.out.println(currentList);
+  }
 
   private static MyLinkedList<Integer> connectLists(MyLinkedList<Integer>[] lists) {
     MyLinkedList<Integer> finalList = new MyLinkedList<>(); //starting list
@@ -61,13 +89,23 @@ public class Radix {
     return finalList;
   }
 
+  private static void initializeBuckets(MyLinkedList<Integer>[] buckets) {
+    for (int i = 0; i < buckets.length; i ++) {
+      buckets[i] = new MyLinkedList<Integer>();
+    }
+  }
+
   private static int getDigit(int n, int place) {
     n = Math.abs(n);
-    if (place == 0) return n % 10;
+    if (place == 1) return n % 10;
     else {
+      // System.out.println();
+      // System.out.println((int)(Math.pow(10,place)));
       int holder = n % (int)(Math.pow(10,place));
+      // System.out.println(holder);
       holder -= n % (Math.pow(10,place-1));
-      holder /= Math.pow(10,place);
+      // System.out.println(holder);
+      holder /= Math.pow(10,place-1);
       return holder;
     }
   }
@@ -83,6 +121,9 @@ public class Radix {
   public static void main(String[] args) {
     int[] test = {10,5,-20,-347,75,345,23,0,30,7,2,1,88};
     radixsort(test);
+    // System.out.println(getDigit(321,1));
+    // System.out.println(getDigit(321,2));
+    // System.out.println(getDigit(321,3));
     // System.out.println("15: " + countDigits(15));
     // System.out.println("1: " + countDigits(1));
     // System.out.println("155: " + countDigits(155));
